4e953b5e186437bc0f43914401e79965ec9d3d01
aarsvijand <85252174+aarsvijand@users.noreply.github.com>
2021-06-04 09:45:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const keepAlive = require("./server")
+const keepAlive = require("./server.js")
 
 const prefix = '!';
 
