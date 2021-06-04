b2a9f9a238a4244c67e1e26b15ed108025bc2f95
aarsvijand <85252174+aarsvijand@users.noreply.github.com>
2021-06-04 20:48:58 +0200

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -1,29 +1,20 @@
 module.exports = {
     name: 'help',
-    description: 'Beschikbare commandos',
 
     async run(client, message, args, con) {
         fs.readdir("./Commands/", (err, files) => {
             if(err) console.error(err);
-    
-            let jsfiles = files.filter(f => f.split(".").pop() === "js");
-            if(jsfiles.length <= 0) {
-                console.log("Geen commandos te laden!");
-                return;
-            }
-            
+                
             var namelist = "";
             var desclist = "";
+            var usage = "";
             
             let result = jsfiles.forEach((f, i) => {
                 let props = require(`./${f}`);
                 namelist = props.name;
-                desclist = props.description;
                 message.author.send(`**${namelist}** \n${desclist} \n`);
             });
-     
-
-     
+         
         });
     },
 };
