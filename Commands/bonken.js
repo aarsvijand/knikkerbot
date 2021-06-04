b18a3a29e11161d1b34ad3dbb51cfcd4ae897b20
aarsvijand <85252174+aarsvijand@users.noreply.github.com>
2021-06-04 20:28:34 +0200

    update

---
+++ Commands/bonken.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'bonken',
+    execute(message, args){
+        message.channel.send('De betrunken bunken?');
+    },
+};
---
+++ Commands/wink.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'wink',
+    execute(message, args){
+        message.channel.send('Wonk');
+    },
+};
