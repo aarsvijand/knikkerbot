2a3d4a6df031c2e8bbbf50d0d37dd372a683c941
aarsvijand <85252174+aarsvijand@users.noreply.github.com>
2021-06-04 12:10:41 +0200

    Update kroest.js

--- Commands/kroest.js
+++ Commands/kroest.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'kroest',
     execute(message, args){
-        message.channel.send('Eeeeeasy babyyy');
+        message.channel.send('Eeeeeasy babyyy https://cdn.discordapp.com/attachments/401735527268876291/850315205065703434/kroest-cunt.mp4');
     },
 };
