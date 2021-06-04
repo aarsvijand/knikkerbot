a48a1b01d9be30f07234df1918ca3627dda0207b
aarsvijand <85252174+aarsvijand@users.noreply.github.com>
2021-06-04 20:26:11 +0200

    update

---
+++ Commands/clips.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'clips',
+    execute(message, args){
+        message.channel.send('Hier staan Shappo0 clippies https://clipsgamelab.github.io/#/clips/channels/shappo0/2000-01-01T00:00:00.000Z/2030-12-31T00:00:00.000Z');
+    },
+};
---
+++ Commands/miloubeast.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'miloubeast',
+    execute(message, args){
+        message.channel.send('Wat een beest is het toch ook he');
+    },
+};
