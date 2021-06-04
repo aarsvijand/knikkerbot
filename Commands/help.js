3242555d31fe371154f29b1de525e60d69807d4a
aarsvijand <85252174+aarsvijand@users.noreply.github.com>
2021-06-04 20:37:31 +0200

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -1,3 +1,36 @@
+const Discord = require('discord.js'); 
+const fs = require("fs");
+
+module.exports = {
+    name: 'help',
+    description: 'Beschikbare commandos',
+
+    async run(client, message, args, con) {
+        fs.readdir("./Commands/", (err, files) => {
+            if(err) console.error(err);
+    
+            let jsfiles = files.filter(f => f.split(".").pop() === "js");
+            if(jsfiles.length <= 0) {
+                console.log("Geen commandos te laden!");
+                return;
+            }
+            
+            var namelist = "";
+            var desclist = "";
+            
+            let result = jsfiles.forEach((f, i) => {
+                let props = require(`./${f}`);
+                namelist = props.name;
+                desclist = props.description;
+                message.author.send(`**${namelist}** \n${desclist} \n`);
+            });
+     
+
+     
+        });
+    }
+}
+
 // const { Channel } = require("discord.js")
 
 // const Discord = Require("discord.js");
