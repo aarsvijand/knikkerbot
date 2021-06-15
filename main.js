c124370d08c15f472cd512d11e04286da6aca74e
aarsvijand <85252174+aarsvijand@users.noreply.github.com>
2021-06-15 23:10:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ const fs = require('fs');
 
 client.commands = new Discord.Collection();
 
-
 const commandFiles = fs.readdirSync('./Commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
     const command = require(`./Commands/${file}`);
