3ac4466120f876a3b2bff6d065e04b7941edc8e7
aarsvijand <85252174+aarsvijand@users.noreply.github.com>
2021-07-01 08:18:54 +0200

    Update stand.js

--- Commands/stand.js
+++ Commands/stand.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'stand',
     execute(message, args){
-        message.channel.send('https://drive.google.com/file/d/14ujRmRZMVI-__huTyLJqeCo6j1FzQSS4/view');
+        message.channel.send('https://docs.google.com/spreadsheets/d/14ujRmRZMVI-__huTyLJqeCo6j1FzQSS4/edit#gid=1884941469');
     },
 };
